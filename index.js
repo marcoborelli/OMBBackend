480ab86ba55174880763110da5ebb2bdb4eb543f
Borelli Marco <Borelli.19312>
2024-04-10 11:30:04 +0200

    \index.js: aggiornato entry-point

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 require('dotenv').config()
 
 const valveRoutes = require('./routes/valve')
+const testRoutes = require('./routes/test')
 
 const app = express()
 mongoose.connect(process.env.ATLAS_URI)
@@ -18,4 +19,6 @@ app.listen(port, () => {
 
 
 app.use(express.json()) // Middleware to parse JSON in request body
+//app.use(express.urlencoded({ extended: true }));
 app.use('/api/valves', valveRoutes)
+app.use('/api/tests', testRoutes)
