f0a1dd9cca941ee93d505dcfaefa33259b43da9c
marcoborelli <marco05.borelli@gmail.com>
2024-04-24 21:47:33 +0200

    \routes\valveFamily.js: aggiunti endpoint base per il nuovo schema

---
+++ routes/valveFamily.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+const ValveFamily = require('../models/ValveFamily.js')
+
+
+router.get("/all", async (req, res) => {
+    try {
+        const data = await ValveFamily.find()
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({ error: 'Error retrieving valve families.' })
+    }
+})
+
+router.get("/get/:valveFamilyID", async (req, res) => {
+    const valveFamilyId = req.params.valveFamilyID
+
+    try {
+        const data = await ValveFamily.findById(valveFamilyId)
+
+        if (!data) {
+            return res.status(404).json({ error: 'Valve family not found' })
+        }
+
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({ error: 'Error retrieving valve families.' })
+    }
+})
+
+router.post("/add", async (req, res) => {
+    try {
+        const { _id, bto, runo, eto, btc, runc, etc, img_url } = req.body
+
+        const newValveFamily = new ValveFamily({
+            _id,
+            bto,
+            runo,
+            eto,
+            btc,
+            runc,
+            etc,
+            img_url
+        })
+
+        await newValveFamily.save()
+        res.status(200).json(newValveFamily)
+    } catch (error) {
+        console.error('Error saving valve family to the database:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+router.put('/edit/:valveFamilyID', async (req, res) => {
+    const valveFamilyId = req.params.valveFamilyID
+    const { bto, runo, eto, btc, runc, etc, img_url } = req.body
+
+    //TODO: mettere un controllo regex che id sia valido
+
+    try {
+        const updatedValveFamily = await ValveFamily.findByIdAndUpdate(valveFamilyId, {  bto, runo, eto, btc, runc, etc, img_url }, { new: true })
+
+        if (!updatedValveFamily) {
+            return res.status(404).json({ error: 'Valve family not found' })
+        }
+
+        res.status(200).json(updatedValveFamily)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+router.delete('/del/:valveFamilyID', async (req, res) => {
+    const valveFamilyId = req.params.valveFamilyID
+
+    //TODO: mettere un controllo regex che id sia valido
+
+    try {
+        const deletedValveFamily = await ValveFamily.findByIdAndDelete(valveFamilyId, { new: true })
+        res.status(200).json({ success: (deletedValveFamily !== null) })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+
+module.exports = router
