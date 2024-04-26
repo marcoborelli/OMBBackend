6cb25c85bab8e9c4c2bd23fb6a0f1362fc27c1aa
marcoborelli <marco05.borelli@gmail.com>
2024-04-26 19:38:59 +0200

    \models\ValveFamily.js: generalizzato oggetto theoric_values per scopi futuri

--- models/ValveFamily.js
+++ models/ValveFamily.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
+const ValveValuesSchema = {
+    _id: false,
+    bto: { type: Number, required: true }, //break to open (0)
+    runo: { type: Number, required: true }, //run open (45)
+    eto: { type: Number, required: true }, //end to open (90)
+    btc: { type: Number, required: true }, //break to close (90)
+    runc: { type: Number, required: true }, //run close (45)
+    etc: { type: Number, required: true } //end to close (0)
+}
+
 const valveFamilySchema = new mongoose.Schema({
     _id: { type: String, required: true, trim: true }, //start of code
     img_url: { type: String },
-    theoric_values: {
-        bto: { type: Number, required: true }, //break to open (0)
-        runo: { type: Number, required: true }, //run open (45)
-        eto: { type: Number, required: true }, //end to open (90)
-        btc: { type: Number, required: true }, //break to close (90)
-        runc: { type: Number, required: true }, //run close (45)
-        etc: { type: Number, required: true } //end to close (0)
-    }
+    theoric_values: { type: ValveValuesSchema, required: true }
 }, {
     versionKey: false
 });
