6aca091140f91cc33402d339229e43234894866d
marcoborelli <marco05.borelli@gmail.com>
2024-04-27 23:03:00 +0200

    \models\ValveInstance.js: aggiornato schema

--- models/ValveInstance.js
+++ models/ValveInstance.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const ValveValuesSchema = require('./ValveValuesSchema');
 
 const valveInstanceSchema = new mongoose.Schema({
     _id: { type: String, required: true, trim: true }, //serial_number
     job_number: { type: String, required: true, trim: true },
     valve_model: { type: String, ref: 'ValveModel', required: true, trim: true },
+    average_values: { type: ValveValuesSchema },
     tests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Test' }]
 }, {
     versionKey: false
