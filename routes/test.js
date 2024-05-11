a1267f31cf29b5581cb84913ee2c040dbc947040
marcoborelli <marco05.borelli@gmail.com>
2024-05-11 21:47:33 +0200

    \routes\test.js: rimosso endpoint inutilizzato

--- routes/test.js
+++ routes/test.js
@@ -6,14 +6,6 @@ const Test = require('../models/Test.js')
 
 const test_for_page = 10
 
-router.get("/info", async (req, res) => {
-    try {
-        res.status(200).json({ elements_number: (await Test.find()).length, elements_for_page: test_for_page })
-    } catch (error) {
-        res.status(500).json({ error: 'Error retrieving tests.' })
-    }
-})
-
 router.get("/all", async (req, res) => {
     let pageNumber = parseInt(req.query.page_number) //le pagine partono da 1
 
